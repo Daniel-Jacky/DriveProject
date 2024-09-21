5d737b3902d6d1662c69545b67b7773df6a14934
Daniel Gilyadov <proxydaniel24@gmail.com>
2024-09-21 23:30:11 +0300

    new params

--- drive/src/components/Home/Home.js
+++ drive/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PlayButton from './PlayButton/PlayButton'
 import ProgressBar from './ProgressBar/ProgressBar';
 import carImage from './Assets/car.png'; // путь к изображению машины
@@ -12,33 +12,52 @@ const Home = ({ onData }) => {
     onData(gameActive); // Вызываем функцию из пропсов и передаем ей данные
   };
 
-  const urlParams = new URLSearchParams(window.location.search);
-const chatId = urlParams.get('chatId');
-const firstName = urlParams.get('firstName');
-const lastName = urlParams.get('lastName');
-const username = urlParams.get('username');
+  const [userData, setUserData] = useState({
+    chatId: '',
+    firstName: '',
+    lastName: '',
+    username: ''
+  });
 
-// Выводим параметры в консоль
-console.log(`Chat ID: ${chatId}, First Name: ${firstName}, Last Name: ${lastName}, Username: ${username}`);
+  // Извлекаем параметры из URL при загрузке компонента
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const chatId = urlParams.get('chatId');
+    const firstName = urlParams.get('firstName');
+    const lastName = urlParams.get('lastName');
+    const username = urlParams.get('username');
+
+    // Устанавливаем полученные данные в state
+    setUserData({
+      chatId,
+      firstName,
+      lastName,
+      username
+    });
+  }, []); // [] пустой массив для того, чтобы useEffect сработал только при монтировании компонента
+
+
+  // Выводим параметры в консоль
+  console.log(`Chat ID: ${userData.chatId}, First Name: ${userData.firstName}, Last Name: ${userData.lastName}, Username: ${userData.username}`);
 
   return (
     <div className="App">
 
       <div className="NameAndStat">
-        <h2 className='User'> Telegramm User</h2>
+        <h2 className='User'> {userData.firstName || 'Guest'}</h2>
         <h2 className='Points'>10.000 Points</h2>
       </div>
 
       <img
-      src={carImage}
-      alt="Player Car"
-      className="player-car"
-    />
+        src={carImage}
+        alt="Player Car"
+        className="player-car"
+      />
 
       <div className="playArea">
         <div className='dropGameBox'>
-        <h3 className='dropGame'>Drop game</h3>
-        <h4 className='timeToPlay'>5</h4>
+          <h3 className='dropGame'>Drop game</h3>
+          <h4 className='timeToPlay'>5</h4>
         </div>
         <div className="roadline"></div> {/* Добавляем линию дороги */}
         <PlayButton onClick={sendDataToParent} className="playBtn">
