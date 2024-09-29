4156ac647000b620a9ebf6360c822ec203a12e3e
Daniel Gilyadov <proxydaniel24@gmail.com>
2024-09-30 02:16:49 +0300

    ю

--- drive/src/components/Home/Game.js
+++ drive/src/components/Home/Game.js
@@ -25,7 +25,7 @@ function Game({ onGameStatus }) {
   const [bubbles, setBubbles] = useState([]);
   const [explosions, setExplosions] = useState([]);
   const [score, setLocalScore] = useState(0);
-  const [timeLeft, setTimeLeft] = useState(15);
+  const [timeLeft, setTimeLeft] = useState(30);
   const [playerPosition, setPlayerPosition] = useState({ x: window.innerWidth / 2, y: window.innerHeight / 1.3 });
   const [gameOver, setGameOver] = useState(false);
   const { setScore } = useUser();
@@ -95,7 +95,7 @@ function Game({ onGameStatus }) {
     setBubbles([]);
     setExplosions([]);
     setLocalScore(0);
-    setTimeLeft(15);
+    setTimeLeft(30);
     setPlayerPosition({ x: window.innerWidth / 2, y: window.innerHeight / 1.5 });
     
   };
