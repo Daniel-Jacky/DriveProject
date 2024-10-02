88046b00a4ecaee2226ec41f87a058f65f68039a
Daniel Gilyadov <proxydaniel24@gmail.com>
2024-10-02 15:56:40 +0300

    first api

--- drive/src/components/Home/Home.js
+++ drive/src/components/Home/Home.js
@@ -6,7 +6,7 @@ import './Home.css';
 import { useUser } from './UserContext'; // Импортируем контекст пользователя
 
 const Home = ({ onGameStatus }) => {
-    const { username, setUsername, chatId, setChatId, score, avatar, setAvatar } = useUser(); // Получаем данные пользователя и функции для их обновления
+    const { username, setUsername, chatId, setChatId, score, setScore, avatar, setAvatar } = useUser(); // Получаем данные пользователя и функции для их обновления
     const [generatedAvatar, setGeneratedAvatar] = useState('');
     const [apiData, setApiData] = useState(null); // Состояние для хранения данных из API
 
@@ -16,51 +16,60 @@ const Home = ({ onGameStatus }) => {
     };
 
     useEffect(() => {
-        // Если chatId уже установлен, не выполняем код
-        if (chatId) return;
+        // Функция для получения данных из API
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://fudg-test2.ru/users');
+                setApiData(response.data); // Устанавливаем полученные данные в состояние
+            } catch (error) {
+                console.error('Ошибка при получении данных:', error);
+            }
+        };
 
-        // Получаем хэш-часть URL
-        const hash = window.location.hash;
-        const paramsString = hash.slice(1);
-        const params = new URLSearchParams(paramsString);
-        const newChatId = params.get('/?chatId'); 
-        const newUsername = params.get('username');
-        const newAvatar = params.get('avatarUrl');
+        fetchData(); // Вызов функции для получения данных
+    }, []); // Пустой массив зависимостей, чтобы вызвать один раз при монтировании компонента
 
-        // Устанавливаем данные в контекст
-        setChatId(newChatId);
-        setUsername(newUsername);
-        if (newAvatar) {
-            setAvatar(newAvatar);
-        } else {
-            const avatarUrl = generateAvatar(newUsername);
-            setAvatar(avatarUrl);
-            setGeneratedAvatar(avatarUrl); // Устанавливаем сгенерированную аватарку
+
+    useEffect(() => {
+        if (apiData) {
+            const data = apiData
+            const hash = window.location.hash;
+            const paramsString = hash.slice(1);
+            const params = new URLSearchParams(paramsString);
+            let newChatId = params.get('/?chatId');
+            // const newUsername = params.get('username');
+            const newAvatar = params.get('avatarUrl');
+            let newUsername = '';
+            let newScore = '';
+    
+            for (let i = 0; i < data.length; i++) {
+                if(newChatId = data[i].chatId){
+                    newUsername = data[i].username
+                    newScore = data[i].score
+                }
+            }
+            // Устанавливаем данные в контекст
+            setChatId(newChatId);
+            setUsername(newUsername);
+            setScore(newScore)
+            if (newAvatar) {
+                setAvatar(newAvatar);
+            } else {
+                const avatarUrl = generateAvatar(newUsername);
+                setAvatar(avatarUrl);
+                setGeneratedAvatar(avatarUrl); // Устанавливаем сгенерированную аватарку
+            }
+    
+            console.log(`Chat ID: ${newChatId}, Username: ${newUsername}`);
         }
-
-        console.log(`Chat ID: ${newChatId}, Username: ${newUsername}`);
-    }, [setChatId, setUsername, chatId]);
+      }, [apiData, setChatId, setUsername, chatId]);
 
     const generateAvatar = (username) => {
         if (!username) return `https://dummyimage.com/100/cccccc/ffffff.png&text=?`;
         const firstLetter = username.charAt(0).toUpperCase();
         return `https://ui-avatars.com/api/?name=${firstLetter[0]}&background=random`;
     };
-
-    useEffect(() => {
-        // Функция для получения данных из API
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://fudg-test2.ru/users');
-                setApiData(response.data); // Устанавливаем полученные данные в состояние
-                debugger
-            } catch (error) {
-                console.error('Ошибка при получении данных:', error);
-            }
-        };
-        
-        fetchData(); // Вызов функции для получения данных
-    }, []); // Пустой массив зависимостей, чтобы вызвать один раз при монтировании компонента
+    console.log(apiData)
 
     return (
         <div className="App">
@@ -87,16 +96,46 @@ const Home = ({ onGameStatus }) => {
                     Play
                 </PlayButton>
             </div>
-            {/* Пример вывода данных из API */}
-            {apiData && (
-                <div className="apiData">
-                    <h3>Данные из API:</h3>
-                    <pre>{JSON.stringify(apiData, null, 2)}</pre>
-                </div>
-            )}
             {/* <ProgressBar /> */}
         </div>
     );
 }
 
 export default Home;
+
+
+
+
+
+    // useEffect(() => {
+    //     // Если chatId уже установлен, не выполняем код
+    //     if (chatId) return;
+
+    //     // Получаем хэш-часть URL
+    //     const hash = window.location.hash;
+    //     const paramsString = hash.slice(1);
+    //     const params = new URLSearchParams(paramsString);
+    //     let newChatId = params.get('/?chatId');
+    //     // const newUsername = params.get('username');
+    //     const newAvatar = params.get('avatarUrl');
+    //     let newUsername = '';
+
+    //     for (let i = 0; i < apiData.length; i++) {
+    //         if(newChatId = apiData[i].chatId){
+    //             newUsername = apiData[i].username
+    //         }
+    //     }
+
+    //     // Устанавливаем данные в контекст
+    //     setChatId(newChatId);
+    //     setUsername(newUsername);
+    //     if (newAvatar) {
+    //         setAvatar(newAvatar);
+    //     } else {
+    //         const avatarUrl = generateAvatar(newUsername);
+    //         setAvatar(avatarUrl);
+    //         setGeneratedAvatar(avatarUrl); // Устанавливаем сгенерированную аватарку
+    //     }
+
+    //     console.log(`Chat ID: ${newChatId}, Username: ${newUsername}`);
+    // }, [setChatId, setUsername, chatId]);
