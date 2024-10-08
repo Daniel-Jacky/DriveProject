0ff8b53998b10df1f169112c1ef058cbdd7f2fd4
Daniel Gilyadov <proxydaniel24@gmail.com>
2024-10-08 18:32:11 +0300

    5.3.13

--- drive/src/components/Home/Home.js
+++ drive/src/components/Home/Home.js
@@ -28,80 +28,84 @@ const Home = ({ }) => {
             const hash = window.location.hash;
             const params = new URLSearchParams(hash.slice(1));
             let newChatId = params.get('/?chatId') || chatId;
-// ref
+            // ref
             debugger
             const decodedUrl = decodeURIComponent(hash);
             const startParamPos = decodedUrl.indexOf('start_param=');
 
             // Если параметр найден, извлекаем его значение
-                    if (startParamPos !== -1) {
-                        // Извлекаем всё после 'start_param='
-                        const startParamValue = decodedUrl.substring(startParamPos + 'start_param='.length);
-                        
-                        // Если есть дополнительные параметры, обрезаем строку до следующего параметра
-                        const endPos = startParamValue.indexOf('&');
-                        const startParam = endPos !== -1 ? startParamValue.substring(0, endPos) : startParamValue;
-                        
-                        console.log('start_param:', startParam); // Выведет "ref_aedbb335-473b-439d-9ec2-860fc46ebea5"
-                        if (startParam && startParam.startsWith('ref_')) {
-                            const refCode = startParam.substring(4); // Извлекаем значение после "ref_"
-                            console.log('Referral code:', refCode);
+            const user = await getUserByChatId(newChatId);    // Проверим, что chatId присутствует в базе, если не присутвует создаем по реф ссылке
+            if (user.chatId === newChatId) {
+                setApiData(user)
+            } else if (startParamPos !== -1) {
+                // Извлекаем всё после 'start_param='
+                const startParamValue = decodedUrl.substring(startParamPos + 'start_param='.length);
 
-                            // 1. Извлекаем параметр tgWebAppData
-                            const params1 = new URLSearchParams(hash);
-                            const tgWebAppData = params1.get('#/?tgWebAppData');
+                // Если есть дополнительные параметры, обрезаем строку до следующего параметра
+                const endPos = startParamValue.indexOf('&');
+                const startParam = endPos !== -1 ? startParamValue.substring(0, endPos) : startParamValue;
 
-                            // 2. Декодируем первый уровень URL-кодировки
-                            const decodedData = decodeURIComponent(tgWebAppData);
+                if (startParam && startParam.startsWith('ref_')) {
+                    const refCode = startParam.substring(4); // Извлекаем значение после "ref_"
+                    console.log('Referral code:', refCode);
 
-                            // 3. Убираем префикс 'user='
-                            const cleanData = decodedData.replace('user=', '');
+                    // 1. Извлекаем параметр tgWebAppData
+                    const params1 = new URLSearchParams(hash);
+                    const tgWebAppData = params1.get('#/?tgWebAppData');
 
-                            // 4. Декодируем второй уровень URL-кодировки
-                            const userDataString = decodeURIComponent(cleanData);
+                    // 2. Декодируем первый уровень URL-кодировки
+                    const decodedData = decodeURIComponent(tgWebAppData);
 
-                            // 5. Отрезаем лишние данные после JSON (находим первый '&')
-                            const jsonPart = userDataString.split('&')[0];
+                    // 3. Убираем префикс 'user='
+                    const cleanData = decodedData.replace('user=', '');
 
-                            // 6. Преобразуем строку в объект и извлекаем username
+                    // 4. Декодируем второй уровень URL-кодировки
+                    const userDataString = decodeURIComponent(cleanData);
 
-                            let username;
-                            let firstname;
-                            let lastname;
-                            try {
-                                const userData = JSON.parse(jsonPart);
-                                username = userData.username;
-                                firstname = userData.first_name
-                                lastname = userData.lastname
-                                console.log(username); // Выведет 'danielgi97'
-                            } catch (e) {
-                                console.error('Ошибка парсинга JSON:', e);
-                            }
-            
-                            // Здесь можно вызвать метод для отправки этого параметра на сервер
-                            // Например, через API или использование в Telegram Web App логике
-                            const newAvatar = params.get('avatarUrl');
-                            const addRed = await addRefFriend(newChatId, firstname, lastname, username, newAvatar, refCode);
-                            console.log(addRed)
-                        } else {
-                            console.log('Параметр ref_ не найден.');
-                        }
-                        
-                    } else {
-                        console.log('Параметр start_param не найден.');
+                    // 5. Отрезаем лишние данные после JSON (находим первый '&')
+                    const jsonPart = userDataString.split('&')[0];
+
+                    // 6. Преобразуем строку в объект и извлекаем username
+
+                    let username;
+                    let firstname;
+                    let lastname;
+                    try {
+                        const userData = JSON.parse(jsonPart);
+                        username = userData.username;
+                        firstname = userData.first_name
+                        lastname = userData.lastname
+                        console.log(username); // Выведет 'danielgi97'
+                    } catch (e) {
+                        console.error('Ошибка парсинга JSON:', e);
                     }
-// ref
-            if (newChatId) {
-                const user = await getUserByChatId(newChatId); // Получаем данные пользователя
-                setApiData(user); // Устанавливаем данные пользователя
+
+
+                    // Здесь можно вызвать метод для отправки этого параметра на сервер
+                    // Например, через API или использование в Telegram Web App логике
+                    const newAvatar = params.get('avatarUrl');
+                    const addRef = await addRefFriend(newChatId, firstname, lastname, username, newAvatar, refCode);
+                    setApiData(addRef)
+                } else {
+                    console.log('Параметр ref_ не найден.');
+                }
+
+            } else {
+                console.log('Параметр start_param не найден.');
             }
+            // ref
+
+
+            // if (newChatId) {
+            //     const user = await getUserByChatId(newChatId); // Получаем данные пользователя
+            //     setApiData(user); // Устанавливаем данные пользователя
+            // }
             setIsLoadingSkeleton(false); // Убираем состояние загрузки скелетона
 
 
             // Получаем параметр из ссылки
-            // const urlParams = new URLSearchParams(window.location.hash);
             // const urlParams = 'tgWebAppData=user%3D%257B%2522id%2522%253A6578624309%252C%2522first_name%2522%253A%2522Daniel%2522%252C%2522last_name%2522%253A%2522%2522%252C%2522username%2522%253A%2522danielgi97%2522%252C%2522language_code%2522%253A%2522en%2522%252C%2522allows_write_to_pm%2522%253Atrue%257D%26chat_instance%3D-1023297304681969391%26chat_type%3Dprivate%26start_param%3Dref_aedbb335-473b-439d-9ec2-860fc46ebea5%26auth_date%3D1728337660%26hash%3Dd4f93ccb815f9a6a9cf50f18871fc4dea72d14029f0321d9013f7dadf6132e39&tgWebAppVersion=7.10&tgWebAppPlatform=weba&tgWebAppThemeParams=%7B%22bg_color%22%3A%22%23ffffff%22%2C%22text_color%22%3A%22%23000000%22%2C%22hint_color%22%3A%22%23707579%22%2C%22link_color%22%3A%22%233390ec%22%2C%22button_color%22%3A%22%233390ec%22%2C%22button_text_color%22%3A%22%23ffffff%22%2C%22secondary_bg_color%22%3A%22%23f4f4f5%22%2C%22header_bg_color%22%3A%22%23ffffff%22%2C%22accent_text_color%22%3A%22%233390ec%22%2C%22section_bg_color%22%3A%22%23ffffff%22%2C%22section_header_text_color%22%3A%22%23707579%22%2C%22subtitle_text_color%22%3A%22%23707579%22%2C%22destructive_text_color%22%3A%22%23e53935%22%7D"';
-           
+
         };
 
         fetchData(); // Вызов асинхронной функции
@@ -186,7 +190,7 @@ const Home = ({ }) => {
     return (
         <SkeletonTheme baseColor="#8b8b8b" highlightColor="#f0f0f0">
             <div className="App">
-                <h4>5.3.12</h4>
+                <h4>5.3.13</h4>
                 <div className="NameAndStat">
                     <div className="user-info">
                         <h2 className="User">
