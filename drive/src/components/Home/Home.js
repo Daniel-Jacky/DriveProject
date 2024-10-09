e3b6f5d70a200d99d4fd8f8576098927055ba074
Daniel Gilyadov <proxydaniel24@gmail.com>
2024-10-09 12:43:14 +0300

    6.4.15

--- drive/src/components/Home/Home.js
+++ drive/src/components/Home/Home.js
@@ -189,7 +189,7 @@ const Home = ({ }) => {
     return (
         <SkeletonTheme baseColor="#8b8b8b" highlightColor="#f0f0f0">
             <div className="App">
-                <h4>5.4.15</h4>
+                <h4>6.4.15</h4>
                 <div className="NameAndStat">
                     <div className="user-info">
                         <h2 className="User">
