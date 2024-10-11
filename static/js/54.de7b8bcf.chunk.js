cf93ef959f5022e10674a9f22279c2c0f1d73e50
Daniel Gilyadov <proxydaniel24@gmail.com>
2024-10-11 11:45:43 +0300

    11.5.18

--- static/js/54.de7b8bcf.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdrive=self.webpackChunkdrive||[]).push([[54],{54:(e,r,a)=>{a.r(r),a.d(r,{default:()=>l});a(43);var s=a(911),t=a(579);const l=function(e){let{x:r,y:a}=e;return(0,t.jsx)("img",{src:s,alt:"Player Car",className:"player-car",style:{left:r,top:a}})}}}]);
-//# sourceMappingURL=54.de7b8bcf.chunk.js.map
